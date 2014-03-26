d91523decb1b7c414b4b1371370d56e0b7f21363
Liu Wei <wliu272@163.com>
2014-03-26 23:42:41 +0800

    preview recaptcha image

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,16 @@
 // I don't like coffee scripts because they are UGLY!
 // Use decent JS instead
+$(document).ready(function(){
+	recaptcha_preview();
+});
+
+function recaptcha_preview() {
+  $('div#recaptcha_image').popover({
+    html: true,
+    trigger: 'hover',
+    placement: 'right',
+    content: function () {
+      return '<img src="' + $($(this).children('img')[0]).attr('src') + '" />';
+    }
+  });
+}
