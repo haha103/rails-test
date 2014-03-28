9063c742f64d74a23c6ab153c495558f5a56a018
Liu Wei <wliu272@163.com>
2014-03-28 09:21:23 +0800

    started to implement form validations for users/new

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,8 +1,20 @@
 // I don't like coffee scripts because they are UGLY!
 // Use decent JS instead
 $(document).ready(function(){
-	recaptcha_preview();
+	switch (location.pathname) {
+	case "/users/new":
+		recaptcha_preview();
+		users_new_form_validate();
+		break;
+	default:
+		break;
+	}
 });
+
+function users_new_form_validate() {
+	$('form#haha-form-signup').validate({
+	});
+}
 
 function recaptcha_preview() {
   $('div#recaptcha_image').popover({
